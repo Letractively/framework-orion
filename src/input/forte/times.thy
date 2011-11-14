43671ec43ba91ad89a8786b3fa5687c33ab3cf7c
felipeleao88 <felipeleao88@0728d659-5c1d-0e7a-f403-e6799d85ca89>
2011-11-14 14:59:55 +0000

    Criação de nova ontologia limitada dos times do rio. Edição de comentários JAVADOC

--- src/input/forte/times.thy
+++ src/input/forte/times.thy
@@ -1,6 +1,4 @@
 ex_Jogador(A) :- jogador(A), fdt:naotitular(A).
-apelido_Time(A) :- fdt:apelido(A), fdt:naoapelido_Torcida(A), fdt:refereseTime(A,B), fdt:time(B).
-presidente(A) :- funcionario(A), fdt:naotecnico(A), fdt:comandaClube(A,B), fdt:time(B).
 membro_Comissao_Tecnica(A) :- funcionario(A), fdt:naotecnico(A), fdt:naopresidente(A), fdt:trabalhaEm(A,B), fdt:time(B).
 funcionario(A) :- fdt:pessoa(A).
 jogador(A) :- fdt:pessoa(A), fdt:jogaEmTime(A,B), fdt:time(B).
